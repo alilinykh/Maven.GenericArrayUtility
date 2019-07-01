131c374cf66a2dad548d50b78bdf072502b76d44
alilinykh <aililnykh@dtcc.edu>
2019-07-01 13:59:01 -0400

    created methods

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -3,5 +3,27 @@ package com.zipcodewilmington.arrayutility;
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility <T> {
+    T[] t;
+
+    public ArrayUtility(T[] t) {
+        this.t = t;
+    }
+
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
+        return null;
+    }
+
+    public Integer getNumberOfOccurrences(T valueToEvaluate) {
+        return null;
+    }
+
+    public T getMostCommonFromMerge(T[] arrayToMerge) {
+        return null;
+    }
+
+
+    public T []removeValue(T valueToRemove) {
+        return null;
+    }
 }
